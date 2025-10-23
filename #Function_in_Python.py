8fa3b81519746d67c48e03029fe6baee0f49a767
TARUN2K3 <tarunsharma2k3@gmail.com>
2025-10-23 23:11:08 +0530

    Function

---
+++ #Function_in_Python.py
@@ -0,0 +1,13 @@
+#Function in Python
+'''Function is the block of reusable code that perform specific task it always starts with "def" keywords
+    Main use: 
+    1.Reusability
+    2.Modularity
+    3.Readability
+    4.Maintainablity
+    
+    There are 3 types of Function:
+    1. Builtin Function : Predefine by python
+    2. User-define Function : Created by user by using def keyword
+    3. Lambda Function : Anonymous one line Function using lambda keyword
+    ''' 
